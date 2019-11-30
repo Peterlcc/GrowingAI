dc731bbad14785e213d214ab729ab46100fd58b1
e <1414499216@qq.com>
2019-11-30 20:13:21 +0800

    首页图片替换

--- src/test/java/com/peter/code/CustomTest.java
+++ src/test/java/com/peter/code/CustomTest.java
@@ -1,6 +1,7 @@
 package com.peter.code;
 
 import com.peter.utils.RegexUtils;
+import org.jasypt.util.text.BasicTextEncryptor;
 
 public class CustomTest {
     public static void main(String[] args) {
@@ -8,5 +9,13 @@ public class CustomTest {
         String number="123456";
         System.out.println(RegexUtils.isEmail(email));
         System.out.println(RegexUtils.isMatch("\\d+",number));
+        BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+        //加密所需的salt(盐)
+        textEncryptor.setPassword("growningai");
+        //要加密的数据（数据库的用户名或密码）
+        String username = textEncryptor.encrypt("root");
+        String password = textEncryptor.encrypt("973126");
+        System.out.println("username:"+username);
+        System.out.println("password:"+password);
     }
 }
